7512eb775ccb0600b1be119e7bb38fdcad6868e0
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2026-06-26 14:47:58 -0500

    Trabajos dejados en clase

---
+++ trabajos/pedido.js
@@ -0,0 +1,17 @@
+let cliente = "Mishelle";
+let ciudad = "Fusagasugá";
+let rappiPrime = false;
+
+console.log(cliente +", el destino de tú pedido es en", ciudad, "\n¡Tienes descuento si eres parte de Rappi Prime!. \n Estado de convenio:", rappiPrime);
+
+let listaPedido = ["Leche", "Huevos", "Panela"];
+console.log("Tus productos son:" , listaPedido);
+console.log(listaPedido[0]);
+
+listaPedido.push("Brownie con helado");
+console.log(listaPedido);
+
+listaPedido.pop();
+console.log(listaPedido);
+
+console.log(listaPedido.length);
