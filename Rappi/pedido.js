7f374db0af2a9a93de03ab7b7fdd0355467e444a
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2026-07-05 13:36:31 -0500

    Finalizado el taller - Parte 4

--- Rappi/pedido.js
+++ Rappi/pedido.js
@@ -1,8 +1,10 @@
-/* PARTE 1:
+/* PARTE 1: Los datos del cliente
     Declaré el nombre como constante porque es el mismo cliente, puede cmbiar de domicilio
     e incluso unirse a Rappi Prime, pero su nombre siempre es el mismo.
     Aquí utilcé un OBJECT porque son datos diferentes que pertenecen a un mismo cliente
 */
+console.log("\nPARTE 1\n");
+
 const nombre = "Mishelle";
 let cliente = {
     nombre,
@@ -11,9 +13,11 @@ let cliente = {
 }
 console.log(cliente.nombre + ", el destino de tú pedido es en" , cliente.ciudad , "\n¡Tienes descuento si eres parte de Rappi Prime!. \nEstado de convenio:" , cliente.rappiPrime);
 
-/* PARTE 2:
+/* PARTE 2: Los productos del pedido
     Aquí se utiliza un ARRAY porque pertenecen a una sola categoria "Productos"
 */
+console.log("\nPARTE 2\n");
+
 let listaPedido = ["Leche", "Huevos", "Panela"];
 console.log("Tus productos son:" , listaPedido);
 console.log("Tu primer producto es: " + listaPedido[0]);
@@ -26,8 +30,10 @@ console.log(listaPedido);
 
 console.log(listaPedido.length); // Cuenta
 
-/* PARTE 3:
+/* PARTE 3: El pedido completo como una ficha
 */
+console.log("\nPARTE 3\n");
+
 let pedido = {
     nombre,
     ciudad: cliente.ciudad,
@@ -49,3 +55,17 @@ console.log(pedido);
 
 // Primer producto
 console.log("Tu primer producto es:" , pedido.productos[0])
+
+/* PARTE 4: El cobro
+*/
+console.log("\nPARTE 4\n");
+
+let subtotal = Number("20000");
+const propina =  subtotal * 0.10;
+let domicilio = 3500;
+let total = subtotal + domicilio + propina;
+
+console.log("Subtotal: " + subtotal);
+console.log("Domicilio: " + domicilio);
+console.log("Propina: " + propina);
+console.log("Total a pagar por el pedido de " + nombre + ": $" + (total));
